1d4d6be4f0ce9b544729152b603bd61fd4c54369
Anand Shah <annshah@amazon.lu>
2021-07-26 10:50:26 +0200

    Removed all existing files

--- benchmarking-tool/cli-wizard-typescript/src/lib/validators/platform.ts
+++ benchmarking-tool/cli-wizard-typescript/src/lib/validators/platform.ts
@@ -4,5 +4,5 @@ import Joi from 'joi';
  * The `Joi` schema for validating the network configuration.
  */
 export const platformSchema = Joi.object().keys({
-  useVpcEndpoints: Joi.boolean().default(false).required()
+
 });
