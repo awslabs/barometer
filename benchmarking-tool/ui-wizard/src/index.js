6d88a3f1ea88f9aeecf9918f922b242e0e1cf856
Anand Shah <annshah@amazon.lu>
2021-07-27 14:43:27 +0200

    React app UI-Wizard init

--- benchmarking-tool/ui-wizard/src/index.js
+++ benchmarking-tool/ui-wizard/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
