72741be2c632e092ae70bb5bc14c05a681afd7c4
Anand Shah <annshah@amazon.lu>
2021-07-30 14:14:10 +0200

    Wizard UI init

--- benchmarking-tool/ui-wizard/src/App.js
+++ benchmarking-tool/ui-wizard/src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import StepZilla from "react-stepzilla";
+import StepWorkload from './Components/stepWorkload'
+import StepPlatform from './Components/stepPlatform'
+import StepExperiment from './Components/stepExperiment'
+
+const steps = [
+    {name: 'Workload', component: <StepWorkload/>},
+    {name: 'Platform', component: <StepPlatform/>},
+    {name: 'Experiment Summary', component: <StepExperiment/>}
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <div className='step-progress'>
+                <StepZilla steps={steps} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
