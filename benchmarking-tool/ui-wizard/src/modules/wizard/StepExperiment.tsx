52278eef6010b05f632574d187b165edfc9b998b
Anand Shah <annshah@amazon.lu>
2021-08-02 17:42:41 +0200

    State change event handling

--- benchmarking-tool/ui-wizard/src/modules/wizard/StepExperiment.tsx
+++ benchmarking-tool/ui-wizard/src/modules/wizard/StepExperiment.tsx
@@ -7,10 +7,32 @@ interface StepExperimentProps {
 }
 
 interface StepExperimentState {
-
+    executionMode: string;
+    concurrentSessionCount: number;
+    keepInfrastructure: boolean;
 }
 
 export default class StepExperiment extends Component<StepExperimentProps, StepExperimentState> {
+
+    constructor(props: StepExperimentProps) {
+        super(props);
+
+        this.state = {
+            concurrentSessionCount: 1,
+            executionMode: "sequential",
+            keepInfrastructure: false
+        }
+    }
+
+    handleChange = (event: React.FormEvent) => {
+        const target = event.target as HTMLInputElement
+        let value: any = target.value;
+        this.setState({
+            ...this.state,
+            [target.id as any]: value
+        });
+        console.log(this.state)
+    }
 
     render() {
         return (
@@ -22,13 +44,22 @@ export default class StepExperiment extends Component<StepExperimentProps, StepE
                             <h6>Benchmarking tool UI Wizard will help you define your experiment</h6>
                         </Form.Label>
                     </Form.Group>
-                    <Form.Group controlId="numberOfQueries">
+                    <Form.Group controlId="concurrentSessionCount">
                         <Form.Label>How many users will run query in parallel?</Form.Label>
-                        <Form.Control required type="number" step={1} min={1} max={10} value={1}/>
+                        <Form.Control required type="number" step={1} min={1} max={10}
+                                      value={this.state.concurrentSessionCount} onChange={this.handleChange}/>
                     </Form.Group>
-                    <Form.Group controlId="numberOfUsers">
-                        <Form.Label>How many queries each user will run concurrently?</Form.Label>
-                        <Form.Control required type="number" step={1} min={1} max={22} value={1}/>
+                    <Form.Group controlId="executionMode">
+                        <Form.Label>Do you want users to run query one by one or in parallel?</Form.Label>
+                        <Form.Control value={this.state.executionMode} as="select" onChange={this.handleChange} custom>
+                            <option value="sequential">Sequential</option>
+                            <option value="concurrent">Concurrent</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="keepInfrastructure">
+                        <Form.Check checked={this.state.keepInfrastructure} id="keepInfrastructure" type="switch"
+                                    onChange={this.handleChange}
+                                    label="Keep infrastructure after experiment run"/>
                     </Form.Group>
                     <Form.Group>
                         <Button className="btn btn-prev btn-amzn btn-lg">Run Experiment</Button>
--- benchmarking-tool/ui-wizard/src/modules/wizard/StepPlatform.tsx
+++ benchmarking-tool/ui-wizard/src/modules/wizard/StepPlatform.tsx
@@ -6,10 +6,60 @@ interface StepPlatformProps {
 }
 
 interface StepPlatformState {
-
+    // Common
+    name: string;
+    platformType: string;
+    // Redshift properties
+    nodeType: string;
+    numberOfNodes: number;
+    workloadManager: boolean;
+    concurrencyScaling: boolean;
+    aqua: boolean;
+    spectrum: boolean;
+    // Athena propoerties
+    enforceWorkgroupConfiguration: boolean;
+    bytesScannedCutoffPerQuery: number;
 }
 
 export default class StepPlatform extends Component<StepPlatformProps, StepPlatformState> {
+
+    constructor(props: StepPlatformProps) {
+        super(props);
+
+        this.state = {
+            name: "My platform",
+            platformType: "Redshift",
+            nodeType: "ra3.xplus",
+            numberOfNodes: 2,
+            workloadManager: false,
+            concurrencyScaling: false,
+            aqua: false,
+            spectrum: false,
+            enforceWorkgroupConfiguration: false,
+            bytesScannedCutoffPerQuery: 20 * 1000000
+        }
+    }
+
+    handleChange = (event: React.FormEvent) => {
+        const target = event.target as HTMLInputElement
+        let value: any = target.value;
+        if (target.id === "bytesScannedCutoffPerQuery") value = value * 1000000;
+        this.setState({
+            ...this.state,
+            [target.id as any]: value
+        });
+        console.log(this.state);
+    }
+
+    handleSwitch = (event: React.FormEvent) => {
+        const target = event.target as HTMLInputElement
+        let value: any = target.value === "on";
+        this.setState({
+            ...this.state,
+            [target.id as any]: value
+        });
+        console.log(this.state);
+    }
 
     render() {
         return (
@@ -20,40 +70,68 @@ export default class StepPlatform extends Component<StepPlatformProps, StepPlatf
                             <h5>Step 2: Define Your Platform</h5>
                             <h6>Benchmarking tool UI Wizard will help you define your experiment</h6>
                         </Form.Label>
-                        <Form.Group controlId="name">
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control type="input" required placeholder="My platform"/>
-                            <Form.Text>
-                                You will be able to use by this name later
-                            </Form.Text>
-                        </Form.Group>
-                        <Form.Group controlId="platformType">
-                            <Form.Label>Which platform would you like to configure?</Form.Label>
-                            <Form.Control as="select" custom>
-                                <option selected>Redshift</option>
-                                <option>Athena</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="nodeType">
-                            <Form.Label> Select the node type</Form.Label>
-                            <Form.Control as="select">
-                                <option selected>ra3.xplus</option>
-                                <option>ra3.4xlarge</option>
-                                <option>ra3.16xlarge</option>
-                                <option>dc2.large</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId="numberOfNodes">
-                            <Form.Label htmlFor="nodes">Number of Nodes</Form.Label>
-                            <Form.Control type="number" value="2"/>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Check type="switch" label="Workload manager(WLM)"/>
-                            <Form.Check type="switch" label="Advanced Query Accelerator (AQUA)"/>
-                            <Form.Check type="switch" label="Concurrency Scaling"/>
-                            <Form.Check type="switch" label="Redshift Spectrum"/>
-                        </Form.Group>
                     </Form.Group>
+                    <Form.Group controlId="name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="input" required placeholder="My platform" value={this.state.name}
+                                      onChange={this.handleChange}/>
+                        <Form.Text>
+                            You will be able to use by this name later
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="platformType">
+                        <Form.Label>Which platform would you like to configure?</Form.Label>
+                        <Form.Control as="select" custom value={this.state.platformType}
+                                      onChange={this.handleChange}>
+                            <option>Redshift</option>
+                            <option>Athena</option>
+                        </Form.Control>
+                    </Form.Group>
+                    {this.state.platformType === "Redshift" ?
+                        <div>
+                            <Form.Group controlId="nodeType">
+                                <Form.Label>Select the node type</Form.Label>
+                                <Form.Control as="select" value={this.state.nodeType}
+                                              onChange={this.handleChange}>
+                                    <option>ra3.xplus</option>
+                                    <option>ra3.4xlarge</option>
+                                    <option>ra3.16xlarge</option>
+                                    <option>dc2.large</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group controlId="numberOfNodes">
+                                <Form.Label>Number of Nodes</Form.Label>
+                                <Form.Control type="number" value={this.state.numberOfNodes}
+                                              onChange={this.handleChange}/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Check checked={this.state.workloadManager} onChange={this.handleSwitch}
+                                            type="switch"
+                                            label="Workload manager(WLM)"/>
+                                <Form.Check checked={this.state.workloadManager} onChange={this.handleSwitch}
+                                            type="switch"
+                                            label="Advanced Query Accelerator (AQUA)"/>
+                                <Form.Check checked={this.state.workloadManager} onChange={this.handleSwitch}
+                                            type="switch"
+                                            label="Concurrency Scaling"/>
+                                <Form.Check checked={this.state.workloadManager} onChange={this.handleSwitch}
+                                            type="switch"
+                                            label="Redshift Spectrum"/>
+                            </Form.Group>
+                        </div>
+                        : <div>
+                            <Form.Group controlId="enforceWorkgroupConfiguration">
+                                <Form.Check checked={this.state.enforceWorkgroupConfiguration}
+                                            onChange={this.handleSwitch}
+                                            type="switch"
+                                            label="Enforce Workgroup Configuration"/>
+                            </Form.Group>
+                            <Form.Group controlId="bytesScannedCutoffPerQuery">
+                                <Form.Label>Bytes Scanned Cutoff Per Query in MBs</Form.Label>
+                                <Form.Control type="number" value={this.state.bytesScannedCutoffPerQuery / 1000000}
+                                              onChange={this.handleChange}/>
+                            </Form.Group>
+                        </div>}
                 </Form>
             </div>
         )
