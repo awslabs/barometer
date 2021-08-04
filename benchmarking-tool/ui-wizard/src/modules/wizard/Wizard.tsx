7a41e2a1142638e3060928554c4cb4769ddd7767
Anand Shah <annshah@amazon.lu>
2021-08-04 14:53:16 +0200

    Saving experiments to DynamoDB

--- benchmarking-tool/ui-wizard/src/modules/wizard/Wizard.tsx
+++ benchmarking-tool/ui-wizard/src/modules/wizard/Wizard.tsx
@@ -3,12 +3,18 @@ import StepZilla from "react-stepzilla";
 import StepExperiment, {StepExperimentState} from "./StepExperiment";
 import StepPlatform, {StepPlatformState} from "./StepPlatform";
 import StepWorkload, {StepWorkloadState} from "./StepWorkload";
+import {API, graphqlOperation} from "aws-amplify";
+import {createExperiment, createPlatform, createWorkload} from "../../graphql/mutations";
+import {GraphQLResult} from '@aws-amplify/api-graphql';
+import {CreatePlatformMutation, CreateWorkloadMutation} from "../../API";
+import {Alert} from "react-bootstrap";
 
 interface WizardProps {
 
 }
 
 interface WizardState {
+    showSuccessAlert: Boolean;
     workloadState: StepWorkloadState;
     platformState: StepPlatformState;
     experimentState: StepExperimentState;
@@ -19,6 +25,7 @@ export default class Wizard extends Component<WizardProps, WizardState> {
     constructor(props: WizardState) {
         super(props);
         this.state = {
+            showSuccessAlert: false,
             workloadState: {
                 name: "My workload",
                 dataset: "tpc-ds/v3",
@@ -47,7 +54,24 @@ export default class Wizard extends Component<WizardProps, WizardState> {
     }
 
     finishWizard = () => {
-        console.log(this.state);
+        const mainAPICalls: Array<Promise<GraphQLResult>> = [];
+        mainAPICalls.push(API.graphql(graphqlOperation(createPlatform, {input: this.state.platformState})) as Promise<GraphQLResult>);
+        mainAPICalls.push(API.graphql(graphqlOperation(createWorkload, {input: this.state.workloadState})) as Promise<GraphQLResult>);
+        Promise.all(mainAPICalls)
+            .then((results) => {
+                let promise = API.graphql(graphqlOperation(createExperiment, {
+                    input: {
+                        concurrentSessionCount: this.state.experimentState.concurrentSessionCount,
+                        executionMode: this.state.experimentState.executionMode,
+                        keepInfrastructure: this.state.experimentState.keepInfrastructure,
+                        experimentPlatformId: (results[0].data as CreatePlatformMutation).createPlatform?.id,
+                        experimentWorkloadId: (results[1].data as CreateWorkloadMutation).createWorkload?.id
+                    }
+                })) as Promise<GraphQLResult>;
+                promise.then(() => {
+                    this.setState({...this.state, showSuccessAlert: true});
+                });
+            });
     }
 
     render() {
@@ -80,6 +104,11 @@ export default class Wizard extends Component<WizardProps, WizardState> {
         ];
         return (
             <div className='step-progress col-md-8'>
+                {this.state.showSuccessAlert ?
+                    <Alert variant="success" onClose={() => this.setState({...this.state, showSuccessAlert: false})}
+                           dismissible>
+                        Your experiment saved successfully.
+                    </Alert> : ""}
                 <StepZilla steps={steps} nextButtonCls="btn btn-prev btn-amzn btn-lg pull-right"
                            backButtonCls="btn btn-prev btn-amzn btn-lg pull-right"/>
             </div>
