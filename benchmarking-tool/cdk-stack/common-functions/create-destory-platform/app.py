64da56636fd64c88c790f84026cd2b2da1411325
Anand Shah <annshah@amazon.lu>
2021-08-31 14:26:59 +0200

    Create platform only if doesn't exist

--- benchmarking-tool/cdk-stack/common-functions/create-destory-platform/app.py
+++ benchmarking-tool/cdk-stack/common-functions/create-destory-platform/app.py
@@ -33,23 +33,28 @@ def manage_stack(event):
 
 def create_stack(platform_config, stack_name, token):
     parameters = []
-    for key, value in platform_config["settings"].items():
-        if type(value) == dict:
-            for k, v in value.items():
-                # Nested L1 dictionary maps as camel case
-                parameters.append({"ParameterKey": key + k.capitalize(), "ParameterValue": str(v).lower()})
-        else:
-            parameters.append({"ParameterKey": key, "ParameterValue": str(value)})
-    template_url = "https://" + os.environ["DataBucketName"] + ".s3." + os.environ[
-        "AWS_REGION"] + ".amazonaws.com/platforms/" + platform_config["platformType"] + "/template.json"
-    print("Executing template from: " + template_url)
-    print("Saving task token " + token + " to DynamoDB & creating stack: " + stack_name)
-    cf.create_stack(StackName=stack_name,
-                    TemplateURL=template_url,
-                    Parameters=parameters,
-                    Capabilities=['CAPABILITY_IAM'],
-                    NotificationARNs=[os.environ["StackUpdateTopicArn"]],
-                    Tags=[{"Key": "ManagedBy", "Value": "BenchmarkingStack"}])
+    try:
+        response = cf.describe_stacks(StackName=stack_name)
+        print("Stack " + stack_name + " already exists. Skipping creation.")
+        sfn_respond(stack_name, response)
+    except cf.exceptions.AlreadyExistsException:
+        for key, value in platform_config["settings"].items():
+            if type(value) == dict:
+                for k, v in value.items():
+                    # Nested L1 dictionary maps as camel case
+                    parameters.append({"ParameterKey": key + k.capitalize(), "ParameterValue": str(v).lower()})
+            else:
+                parameters.append({"ParameterKey": key, "ParameterValue": str(value)})
+        template_url = "https://" + os.environ["DataBucketName"] + ".s3." + os.environ[
+            "AWS_REGION"] + ".amazonaws.com/platforms/" + platform_config["platformType"] + "/template.json"
+        print("Executing template from: " + template_url)
+        print("Saving task token " + token + " to DynamoDB & creating stack: " + stack_name)
+        cf.create_stack(StackName=stack_name,
+                        TemplateURL=template_url,
+                        Parameters=parameters,
+                        Capabilities=['CAPABILITY_IAM'],
+                        NotificationARNs=[os.environ["StackUpdateTopicArn"]],
+                        Tags=[{"Key": "ManagedBy", "Value": "BenchmarkingStack"}])
 
 
 def handle_stack_delete_complete(record):
@@ -94,10 +99,16 @@ def fetch_and_delete_task_token(stack_name):
 
 def handle_stack_create_complete(record):
     stack_name = extract_stack_name(record)
+    sfn_respond(stack_name)
+
+
+def sfn_respond(stack_name, describe_stack_response=None):
     secret_ids = {"secretIds": [], "stackName": stack_name}
     token = fetch_and_delete_task_token(stack_name)
     if token is not None:
-        stack_response = cf.describe_stacks(StackName=stack_name)
+        stack_response = describe_stack_response
+        if stack_response is None:
+            stack_response = cf.describe_stacks(StackName=stack_name)
         outputs = stack_response["Stacks"][0]["Outputs"]
         for output in outputs:
             if output["OutputKey"].startswith("SecretId"):
