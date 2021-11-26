2383f9cda1f5f4feae4321096ecaf8099cfed4bd
Anand Shah <annshah@amazon.lu>
2021-11-26 14:12:52 +0100

    Project Re-structure

--- benchmarking-tool/cdk-stack-python/aws_redshift/tpc-h/create-tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS region (
-    r_regionkey int4 not null,
-    r_name char(25) not null,
-    r_comment varchar(152) not null,
-    Primary Key(R_REGIONKEY)
-    )
-    DISTKEY(r_regionkey) SORTKEY(r_regionkey);
