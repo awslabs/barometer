70290cccb288f396e1d87c9e0571005709629177
Anand Shah <annshah@amazon.lu>
2022-03-23 17:09:16 +0100

    Bring your own workload, convention test on given s3 path at root level

---
+++ custom-workload/benchmarking-queries/redshift/query-3.sql
@@ -0,0 +1,3 @@
+select *
+from table_1 as names join table_2 as ages
+on names.id = ages.id
--- source/cli-wizard/src/lib/wizard/modules/workload.ts
+++ source/cli-wizard/src/lib/wizard/modules/workload.ts
@@ -6,6 +6,9 @@ import fs from "fs";
 import {WorkloadConfiguration, WorkloadSettings} from "../../impl/workload";
 import {Utils} from "../utils";
 import path = require('path');
+import {CloudFormationClient} from "@aws-sdk/client-cloudformation";
+import {S3} from "@aws-sdk/client-s3";
+import {URL} from "url";
 
 export class Module {
     public static getInstance(configuration: Configuration): WorkloadModule {
@@ -14,6 +17,9 @@ export class Module {
 }
 
 export class WorkloadModule extends CLIModule implements ICLIModule {
+
+    s3Client = new S3({region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION});
+
     /**
      * Questions to be prompted
      */
@@ -24,6 +30,39 @@ export class WorkloadModule extends CLIModule implements ICLIModule {
             type: 'list',
             name: 'dataset',
             message: 'Which dataset would you like to use ?'
+        });
+        this.questions.push({
+            type: 'input',
+            name: 's3Path',
+            message: 'Please provide path to your workload on S3 ?',
+            when: function (answers): boolean {
+                return answers.dataset == "BYOW";
+            },
+            validate: async (input: string): Promise<any> => {
+                const url = new URL(input);
+                try {
+                    let prefix = url.pathname.replace(/^\//g, '');
+                    if (!prefix.endsWith("/"))
+                        prefix += '/';
+                    const result = await this.s3Client.listObjectsV2({
+                        Bucket: url.hostname,
+                        Delimiter: '/',
+                        Prefix: prefix
+                    })
+                    console.log(result);
+                    let count = 0;
+                    if (result.CommonPrefixes)
+                        for (const commonPrefix of result.CommonPrefixes) {
+                            if (commonPrefix.Prefix?.endsWith("ddl/") || commonPrefix.Prefix?.endsWith("volumes/") || commonPrefix.Prefix?.endsWith("benchmarking-queries/"))
+                                count++;
+                        }
+                    if (count != 3)
+                        return "Folders ddl/, volumes/ and benchmarking-queries/ must be present in path " + input;
+                } catch (e) {
+                    return e.message;
+                }
+                return true;
+            }
         });
         this.questions.push({
             type: 'list',
@@ -50,6 +89,7 @@ export class WorkloadModule extends CLIModule implements ICLIModule {
             const config = JSON.parse(fs.readFileSync(workloadDirPath + workloadConfigDirs[i] + "/config.json", 'utf-8'));
             datasetQuestion["choices"].push({name: config.description, value: config});
         }
+        datasetQuestion["choices"].push({name: "Bring your own workload from Amazon S3", value: "BYOW"});
 
         await this.askQuestions(this.getQuestions()).then(async (answers) => {
             if (answers) {
