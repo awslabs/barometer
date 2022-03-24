8ae086185e1f33181760ac0fd24670b71ae6432b
Anand Shah <annshah@amazon.lu>
2022-03-24 12:56:56 +0100

    Removed workloadType, bring your own workload validation and config generation

--- source/cli-wizard/src/lib/wizard/modules/workload.ts
+++ source/cli-wizard/src/lib/wizard/modules/workload.ts
@@ -2,13 +2,13 @@ import {Configuration} from '../../impl/configuration';
 
 import {CLIModule, ICLIModule} from '../common/cli-module';
 import {CLIModuleQuestions} from "../common/cli-prompts";
-import fs from "fs";
 import {WorkloadConfiguration, WorkloadSettings} from "../../impl/workload";
 import {Utils} from "../utils";
-import path = require('path');
-import {CloudFormationClient} from "@aws-sdk/client-cloudformation";
 import {S3} from "@aws-sdk/client-s3";
 import {URL} from "url";
+import {PlatformType} from "../../interface/platform";
+import {IWorkloadSettings} from "../../interface/workload";
+import path = require('path');
 
 export class Module {
     public static getInstance(configuration: Configuration): WorkloadModule {
@@ -19,6 +19,7 @@ export class Module {
 export class WorkloadModule extends CLIModule implements ICLIModule {
 
     s3Client = new S3({region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION});
+    static BYOW = "BYOW";
 
     /**
      * Questions to be prompted
@@ -36,20 +37,19 @@ export class WorkloadModule extends CLIModule implements ICLIModule {
             name: 's3Path',
             message: 'Please provide path to your workload on S3 ?',
             when: function (answers): boolean {
-                return answers.dataset == "BYOW";
+                return answers.dataset == WorkloadModule.BYOW;
             },
-            validate: async (input: string): Promise<any> => {
+            validate: async (input: string, answers): Promise<any> => {
                 const url = new URL(input);
                 try {
                     let prefix = url.pathname.replace(/^\//g, '');
                     if (!prefix.endsWith("/"))
                         prefix += '/';
-                    const result = await this.s3Client.listObjectsV2({
+                    let result = await this.s3Client.listObjectsV2({
                         Bucket: url.hostname,
                         Delimiter: '/',
                         Prefix: prefix
-                    })
-                    console.log(result);
+                    });
                     let count = 0;
                     if (result.CommonPrefixes)
                         for (const commonPrefix of result.CommonPrefixes) {
@@ -58,6 +58,82 @@ export class WorkloadModule extends CLIModule implements ICLIModule {
                         }
                     if (count != 3)
                         return "Folders ddl/, volumes/ and benchmarking-queries/ must be present in path " + input;
+                    else {
+                        // Count = 3, go into details and build workload config
+                        const workloadConfig: IWorkloadSettings = {
+                            name: answers.name,
+                            description: answers.name,
+                            volumes: [],
+                            ddl: {},
+                            queries: {},
+                            supportedPlatforms: []
+                        };
+                        // Go to ddl folder to test supported platforms
+                        result = await this.s3Client.listObjectsV2({
+                            Bucket: url.hostname,
+                            Delimiter: '/',
+                            Prefix: prefix + "ddl/"
+                        });
+                        let platformTypes = "";
+                        if (result.CommonPrefixes) {
+                            const platformMatches = result.CommonPrefixes.every(commonPrefix => {
+                                for (const _key in PlatformType) {
+                                    if (commonPrefix.Prefix?.endsWith(PlatformType[_key] + "/")) {
+                                        workloadConfig.supportedPlatforms.push(PlatformType[_key]);
+                                        workloadConfig.ddl[PlatformType[_key]] = {
+                                            path: "s3://" + url.hostname + "/" + commonPrefix.Prefix
+                                        }
+                                        platformTypes += PlatformType[_key] + ",";
+                                        return true;
+                                    }
+                                }
+                                return false;
+                            });
+                            if (!platformMatches) return "Path " + prefix + "ddl/ should contain only platform types: " + platformTypes;
+                        } else return "Path " + prefix + "ddl/ should contain at least 1 folder matching platform type";
+
+                        // Go to benchmarking-queries folder to test supported platforms
+                        result = await this.s3Client.listObjectsV2({
+                            Bucket: url.hostname,
+                            Delimiter: '/',
+                            Prefix: prefix + "benchmarking-queries/"
+                        });
+                        if (result.CommonPrefixes) {
+                            const platformMatches = result.CommonPrefixes.every(commonPrefix => {
+                                for (const _key in PlatformType) {
+                                    if (commonPrefix.Prefix?.endsWith(PlatformType[_key] + "/") && workloadConfig.supportedPlatforms.indexOf(PlatformType[_key]) > -1) {
+                                        workloadConfig.queries[PlatformType[_key]] = {
+                                            path: "s3://" + url.hostname + "/" + commonPrefix.Prefix
+                                        }
+                                        return true;
+                                    }
+                                }
+                                return false;
+                            });
+                            if (!platformMatches) return "Path " + prefix + "benchmarking-queries/ should contain only platform types: " + platformTypes;
+                        } else return "Path " + prefix + "benchmarking-queries/ should contain at least 1 folder matching platform type";
+
+                        // Go to volumes and add all volumes
+                        result = await this.s3Client.listObjectsV2({
+                            Bucket: url.hostname,
+                            Delimiter: '/',
+                            Prefix: prefix + "volumes/"
+                        });
+                        if (result.CommonPrefixes) {
+                            for (const commonPrefix of result.CommonPrefixes) {
+                                if (commonPrefix.Prefix) {
+                                    const splits = commonPrefix.Prefix.split('/');
+                                    workloadConfig.volumes?.push({
+                                        name: splits[splits.length - 2],
+                                        path: "s3://" + url.hostname + "/" + commonPrefix.Prefix,
+                                        format: "parquet"
+                                    });
+                                } else return "No volumes found in format " + prefix + "volumes/[scale-factor]/[table-name]/"
+                            }
+                            const workloadDirPath: string = path.join(__dirname, '../../../../../cdk-stack/workloads/');
+                            Utils.writeToDir(workloadDirPath + workloadConfig.name, 'config.json', workloadConfig);
+                        } else return "Path " + prefix + "volumes/ should contain at least 1 folder";
+                    }
                 } catch (e) {
                     return e.message;
                 }
@@ -74,6 +150,9 @@ export class WorkloadModule extends CLIModule implements ICLIModule {
                     return {name: e.name, value: e}
                 })
             },
+            when: function (answers): boolean {
+                return answers.dataset != WorkloadModule.BYOW;
+            }
         });
     }
 
@@ -86,13 +165,13 @@ export class WorkloadModule extends CLIModule implements ICLIModule {
         const workloadDirPath: string = path.join(__dirname, '../../../../../cdk-stack/workloads/');
         const workloadConfigDirs = Utils.listPaths(workloadDirPath, true);
         for (let i = 0; i < workloadConfigDirs.length; i++) {
-            const config = JSON.parse(fs.readFileSync(workloadDirPath + workloadConfigDirs[i] + "/config.json", 'utf-8'));
+            const config = Utils.readJson(workloadDirPath + workloadConfigDirs[i] + "/config.json");
             datasetQuestion["choices"].push({name: config.description, value: config});
         }
-        datasetQuestion["choices"].push({name: "Bring your own workload from Amazon S3", value: "BYOW"});
+        datasetQuestion["choices"].push({name: "Bring your own workload from Amazon S3", value: WorkloadModule.BYOW});
 
         await this.askQuestions(this.getQuestions()).then(async (answers) => {
-            if (answers) {
+            if (answers && answers.dataset != WorkloadModule.BYOW) {
                 const settings = new WorkloadSettings();
                 settings.name = answers.dataset.name;
                 settings.description = answers.dataset.description;
@@ -100,8 +179,6 @@ export class WorkloadModule extends CLIModule implements ICLIModule {
                 settings.ddl = answers.dataset.ddl;
                 settings.queries = answers.dataset.queries;
                 settings.supportedPlatforms = answers.dataset.supportedPlatforms;
-                settings.workloadType = answers.dataset.type.toUpperCase();
-
                 const entry = new WorkloadConfiguration();
                 entry.name = answers.name;
                 entry.settings = settings;
