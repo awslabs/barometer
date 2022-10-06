8a01a6a8b2c9c002cf177f0eb063821b8343d966
Anand Shah <annshah@amazon.lu>
2022-10-06 16:40:14 +0200

    Custom workload example for aurora

---
+++ custom-workload/ddl/aurora-serverless/ddl.sql
@@ -0,0 +1,13 @@
+create table if not exists table_1
+(
+    id   int        not null,
+    name varchar(3) not null,
+    primary key (id)
+);
+
+create table if not exists table_2
+(
+    id  int not null,
+    age int not null,
+    primary key (id)
+);
