cfb25b9011c10770e00f1b185761cd745ca88b2e
Anand Shah <annshah@amazon.lu>
2022-12-19 10:48:11 +0100

    Analytics code for CDK

--- source/cdk-stack/bin/benchmarking-infra.ts
+++ source/cdk-stack/bin/benchmarking-infra.ts
@@ -6,6 +6,7 @@ import {BenchmarkingStack} from "../lib/benchmarking-stack";
 
 const app = new cdk.App();
 new BenchmarkingStack(app, 'BenchmarkingStack', {
+    description: "uksb-1tmk781t2",
     env: {
         account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
