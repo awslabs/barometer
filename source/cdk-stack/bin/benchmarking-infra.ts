548766d176763a3d6e4ff0b1b100ce145664b068
Basri Dogan <basri.dogan.71@gmail.com>
2023-06-15 10:24:37 +0100

    suggestion for minor description change

--- source/cdk-stack/bin/benchmarking-infra.ts
+++ source/cdk-stack/bin/benchmarking-infra.ts
@@ -61,7 +61,7 @@ function generatestacks(quicksightAdminRegion: string) {
       }
   });
   const benchmarkingstack = new BenchmarkingStack(app, 'BenchmarkingStack', {
-      description: "uksb-1tmk781t2",
+      description: "(uksb-1tmk781t2)",
       env: {
           account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
           region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION
